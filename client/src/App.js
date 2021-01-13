ed48176572c6fc437cc7877d6a43f735a936098c
Andrew Hodge <hodge.iaj@gmail.com>
2021-01-13 05:38:34 -0800

    First skeleton commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import { MuiThemeProvider } from "@material-ui/core";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import { theme } from "./themes/theme";
-import LandingPage from "./pages/Landing";
+import LandingPage from "./pages/Landing"
+import UserPage from "./pages/User";
 
 import "./App.css";
 
@@ -11,6 +12,7 @@ function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
+        <Route path="/user" component={UserPage} />
         <Route path="/" component={LandingPage} />
       </BrowserRouter>
     </MuiThemeProvider>

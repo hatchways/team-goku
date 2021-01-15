c8d89283d31eecb323496578380b1b785ba09faa
Tony Le <le.tony.05@gmail.com>
2021-01-15 10:42:40 -0500

    Fixed background image for large screens on the right squishing and added Signup Button.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,25 +2,43 @@ import React, { useState } from "react";
 
 import { makeStyles } from '@material-ui/core/styles';
 
-import { TextField, Button, InputLabel, Grid, Hidden } from "@material-ui/core";
+import { TextField, Button, InputLabel, Grid, Hidden, Box, Typography } from "@material-ui/core";
+import { flexbox, positions } from '@material-ui/system';
 import { validateEmail, validatePassword } from "../Validation";
+import LoginComponent from "../LoginComponent";
 
-import image from '../images/0b74a39fcb811ed6c86f3bd4359962d5f5a4bc03.png'
+import image from '../images/ddb3f7c7b2544f7f1c636f0270f032276c911f02.png'
 
 const useStyles = makeStyles({
     root: {
         minHeight: '100vh'
     },
-    // Might need to change for deployment
+    test: {
+    },
     bg: {
+        // Might need to change url for deployment
         backgroundImage: `url(${image})`,
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
-        backgroundSize: '100% 100%'
+        backgroundSize: 'cover 100%'
     }
 });
 
-function Login(props) {
+function SignupButton() {
+    return (
+        <Box
+            display="flex"
+            justifyContent="flex-end"
+            alignItems="baseline">
+            Don't have an account?
+            <Button>
+                SIGN UP
+            </Button>
+        </Box>
+    )
+}
+
+function Login() {
 
     const [email, setEmail] = useState("");
     const [validEmail, setValidEmail] = useState(true);
@@ -45,57 +63,57 @@ function Login(props) {
 
     const classes = useStyles();
     return (
-        <Grid container spaceing={3} className={classes.root}>
-            <Grid item xs container
-                direction="row"
-                justify="space-around"
-                alignItems="center"
-            >
-                <form noValidate autoComplete="off" onSubmit={handleSubmit}>
-                    Login
-                    <InputLabel htmlFor="email-input">
-                        EMAIL
-                    </InputLabel>
-                    <TextField
-                        id="email-input"
-                        name="email"
-                        required
-                        variant="outlined"
-                        onChange={handleEmailInput}
-                        error={!validEmail}
-                        helperText={validEmail === false ? 'Please enter an email address.' : ''}
-
-                    />
-                    <InputLabel htmlFor="password-input">
-                        PASSWORD
-                    </InputLabel>
-                    <TextField
-                        id="password-input"
-                        name="password"
-                        required
-                        type="password"
-                        variant="outlined"
-                        onChange={handlePasswordInput}
-                        error={!validPassword}
-                        helperText={validPassword === false ? 'Password should be at least 6 characters.' : ''}
-                    />
-                    <div>
-                        <Button type="submit">
-                            SIGN IN
-                        </Button>
-                    </div>
-                </form>
-            </Grid>
-            <Hidden smDown>
-                <Grid item xs container
-                    direction="row"
-                    justify="space-around"
-                    alignItems="center"
-                    className={classes.bg}
-                >
-                </Grid>
+        <Box>
+            <Hidden smUp>
+                <SignupButton/>
             </Hidden>
-        </Grid>
+            <Grid container className={classes.root}>
+                <Grid item xs
+                    container
+                    direction="row"
+                    justify="center"
+                    alignItems="center">
+                    <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+                        Login
+                    <InputLabel htmlFor="email-input">
+                            EMAIL
+                    </InputLabel>
+                        <TextField
+                            id="email-input"
+                            name="email"
+                            required
+                            variant="outlined"
+                            onChange={handleEmailInput}
+                            error={!validEmail}
+                            helperText={validEmail === false ? 'Please enter an email address.' : ''}
+                        />
+                        <InputLabel htmlFor="password-input">
+                            PASSWORD
+                    </InputLabel>
+                        <TextField
+                            id="password-input"
+                            name="password"
+                            required
+                            type="password"
+                            variant="outlined"
+                            onChange={handlePasswordInput}
+                            error={!validPassword}
+                            helperText={validPassword === false ? 'Password should be at least 6 characters.' : ''}
+                        />
+                        <div>
+                            <Button type="submit">
+                                SIGN IN
+                        </Button>
+                        </div>
+                    </form>
+                </Grid>
+                <Hidden xsDown>
+                    <Grid item xs className={classes.bg}>
+                        <SignupButton/>
+                    </Grid>
+                </Hidden>
+            </Grid>
+        </Box>
     );
 }
 

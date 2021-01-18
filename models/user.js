cff92a1c09990f5b430530c412fc13ac1e7a8541
Andrew Hodge <hodge.iaj@gmail.com>
2021-01-18 10:33:48 -0800

    user_schema suggestions

--- models/user.js
+++ models/user.js
@@ -1,11 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+const recipeSchema = new Schema({
+
+	id: mongoose.Schema.Types.ObjectId,
+	ingredients: { type:[String], required: true },
+	description: { type: String, required: true},
+	
+});
+
 const userSchema = new Schema({
 
 	id: mongoose.Schema.Types.ObjectId,
 	name: { type: String, required: true } ,
 	email: { type: String, required: true, unique: true } ,
-	password: { type: String, required: true }
+	password: { type: String, required: true },
+	isChef: { type: Boolean, required: true },
+	fav_recipes: [recipeSchema],
+	chef_recipes: [recipeSchema],
+
 
 });
 

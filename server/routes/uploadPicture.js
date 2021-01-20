7fc1b1615a1abb78b29126a05788ced9edc32201
Tony Le <le.tony.05@gmail.com>
2021-01-20 16:36:40 -0500

    Start of profile picture uploading

---
+++ server/routes/uploadPicture.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const upload = require("../services/ImageUpload");
+const singleUpload = upload.single("image");
+
+
+router.post("/uploadpicture", function (req, res) {
+    singleUpload(req, res, function (err) {
+        if (err) {
+          return res.json({
+            success: false,
+            errors: {
+              title: "Image Upload Error",
+              detail: err.message,
+              error: err,
+            },
+          });
+        }
+        // The uploaded file location will need to be stored.
+        console.log(req.file.location);
+
+        res.status(200).send({ success: true, response: "Successful Upload" });
+    });
+});
+
+module.exports = router;
---
+++ server/services/ImageUpload.js
@@ -0,0 +1,46 @@
+const aws = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+
+const s3 = new aws.S3();
+
+require('dotenv').config();
+
+aws.config.update({
+    secretAccessKey: process.env.S3_ACCESS_SECRET,
+    accessKeyId: process.env.S3_ACCESS_KEY,
+    region: "us-east-2",
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+        cb(null, true);
+    }
+    else {
+        cb(new Error("Invalid file type, only JPEG and PNG is allowed!"), false);
+    }
+};
+
+const upload = multer({
+  fileFilter,
+  storage: multerS3({
+    acl: "public-read",
+    s3: s3,
+    bucket: "gokuchefsmenu",
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      var fileExt = "";
+      if (file.mimetype === "image/jpeg") {
+        fileExt = ".jpeg";
+      }
+      else if (file.mimetype === "image/png") {
+        fileExt = ".png";
+      }
+      cb(null, Date.now().toString() + fileExt);
+    },
+  }),
+});
+
+module.exports = upload;

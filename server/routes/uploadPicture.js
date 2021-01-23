5c963a897efe683e24adf09e314731be23eb1a54
Tony Le <le.tony.05@gmail.com>
2021-01-23 10:01:33 -0500

    Left reminder comments

--- server/routes/uploadPicture.js
+++ server/routes/uploadPicture.js
@@ -15,7 +15,8 @@ router.post("/uploadpicture", function (req, res) {
         },
       });
     }
-    // The uploaded file location will need to be stored.
+    // The uploaded file location will need to be stored in the database. 
+    // Also the post request will need to be updated to hold the user id to know which user to update.
     console.log(req.file.location);
 
     res.status(200).send({ success: true, response: "Successful Upload" });

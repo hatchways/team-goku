390f465d5dde6521d0b1c1e4cb3d44fb2642c462
Tony Le <le.tony.05@gmail.com>
2021-01-23 10:02:09 -0500

    Styled the upload dialog

--- client/src/UploadDialog.js
+++ client/src/UploadDialog.js
@@ -2,18 +2,54 @@ import React, { useEffect, useState } from "react";
 import {
   Grid,
   Button,
+  Typography,
+  makeStyles,
   Dialog,
   DialogTitle,
-  makeStyles,
-  useEventCallback,
+  DialogContent,
+  DialogActions,
+  Avatar
 } from "@material-ui/core";
 import { useDropzone } from "react-dropzone";
 
-const themes = makeStyles({
+const themes = makeStyles((theme) => ({
   button: {
     color: "#000",
   },
-});
+  dropzone: {
+    backgroundColor: "#bdbdbd",
+    borderWidth: "2px",
+    borderRadius: "2px",
+    borderColor: "#eeeeee",
+    borderStyle: "dashed",
+    padding: "40px",
+  },
+  dropzoneText: {
+    fontWeight: "bold",
+  },
+  avatar: {
+    width: theme.spacing(25),
+    height: theme.spacing(25),
+  },
+  primaryButton: {
+    backgroundColor: "#FF743D",
+    color: "#fff",
+    textTransform: "none",
+    margin: "1em 0 1em 0",
+    borderRadius: "0 0 0 0",
+    "&:hover": {
+      backgroundColor: "#AD6800",
+    },
+  },
+  secondaryButton: {
+    textTransform: "none",
+    margin: "1em 0 1em 0",
+    borderRadius: "0 0 0 0",
+    "&:hover": {
+      backgroundColor: "#bdbdbd",
+    },
+  },
+}));
 
 function UploadDialog(props) {
   const [files, setFiles] = useState([]);
@@ -28,7 +64,7 @@ function UploadDialog(props) {
   };
 
   const { getRootProps, getInputProps } = useDropzone({
-    accept: "image/*",
+    accept: "image/jpeg, image/png",
     onDrop: (acceptedFiles) => {
       setFiles(
         acceptedFiles.map((file) =>
@@ -41,14 +77,6 @@ function UploadDialog(props) {
     multiple: false,
   });
 
-  const thumbs = files.map((file) => (
-    <div key={file.name}>
-      <div>
-        <img src={file.preview} />
-      </div>
-    </div>
-  ));
-
   const handleImageUpload = (event) => {
     var formdata = new FormData();
     formdata.append("image", files[0]);
@@ -58,21 +86,20 @@ function UploadDialog(props) {
       body: formdata,
     };
 
-    fetch("http://localhost:3001/upload/uploadpicture", requestOptions)
+    //To do: Need to catch errors properly to alert user in case upload goes wrong
+    fetch("http://localhost:5000/upload/uploadpicture", requestOptions)
       .then((response) => response.text())
       .then((result) => console.log(result))
       .catch((error) => console.log("error", error));
-
-    event.preventDefault();
   };
 
-  useEffect(
-    () => () => {
-      // Make sure to revoke the data uris to avoid memory leaks
-      files.forEach((file) => URL.revokeObjectURL(file.preview));
-    },
-    [files]
-  );
+ useEffect(
+   () => () => {
+     // Make sure to revoke the data uris to avoid memory leaks
+     files.forEach((file) => URL.revokeObjectURL(file.preview));
+   },
+   [files]
+ );
 
   const classes = themes();
   return (
@@ -89,14 +116,37 @@ function UploadDialog(props) {
         open={open}
         onClose={handleClose}
       >
-        <DialogTitle id="simple-dialog-title">Title</DialogTitle>
+        <DialogTitle id="simple-dialog-title">
+          Upload a New Profile Picture
+        </DialogTitle>
         <form onSubmit={handleImageUpload}>
-          <div {...getRootProps({ className: "dropzone" })}>
-            <input {...getInputProps()} />
-            <p>Drag 'n' drop some files here, or click to select files</p>
-          </div>
-          <div>{thumbs}</div>
-          <Button type="submit">Upload</Button>
+          <DialogContent>
+            <Grid container direction="column" alignItems="center" spacing={2}>
+              <Grid item>
+                <Button className={classes.dropzone} {...getRootProps()}>
+                  <input {...getInputProps()} />
+                  <Typography className={classes.dropzoneText}>
+                    Drop an image here or click to select an image
+                  </Typography>
+                </Button>
+              </Grid>
+              <Grid item>
+                <Avatar
+                  className={classes.avatar}
+                  src={files.length !== 0 ? files[0].preview : ""}
+                  alt="avatar"
+                />
+              </Grid>
+            </Grid>
+          </DialogContent>
+          <DialogActions>
+            <Button className={classes.secondaryButton} onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button className={classes.primaryButton} type="submit">
+              Upload
+            </Button>
+          </DialogActions>
         </form>
       </Dialog>
     </Grid>

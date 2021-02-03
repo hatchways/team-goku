922cc4f9fede93daa4214e82af8cd3ad2e347e7e
Tony Le <le.tony.05@gmail.com>
2021-02-03 14:41:35 -0500

    more styling on profile

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Grid, Avatar, Button, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import { getLogin } from "../util/LoginLogoutUtils";
 
 const themes = makeStyles((theme) => ({
   root: {
@@ -15,16 +16,16 @@ const themes = makeStyles((theme) => ({
     fontWeight: "500",
   },
   sendMessageButton: {
-    color: "#FF743D",
+    color: "#ff743d",
     borderStyle: "solid",
     borderWidth: "1px",
-    borderColor: "#FF743D",
+    borderColor: "#ff743d",
     textTransform: "none",
     margin: "1em 0 1em 0",
     borderRadius: "0 0 0 0",
     padding: "2em 3em 2em 3em",
     "&:hover": {
-      backgroundColor: "#FF743D",
+      backgroundColor: "#ff743d",
       color: "#fff",
     },
   },
@@ -35,64 +36,92 @@ const themes = makeStyles((theme) => ({
     fontWeight: "500",
     color: "#777777",
   },
+  cuisine: {
+    color: "#ffffff",
+    backgroundColor: "#ff743d",
+    margin: "4px 4px 4px 4px",
+    textTransform: "uppercase",
+    fontSize: "0.8em",
+    fontWeight: "600",
+  },
 }));
 
 function UserProfile() {
-    const classes = themes();
-    
-    return (
-      <Grid>
-        <Grid container spacing={2}>
-          <Grid item sm={4}>
-            <Grid
-              container
-              direction="column"
-              justify="center"
-              alignItems="center"
-            >
-              <Grid item>
-                <Avatar className={classes.avatar}></Avatar>
-              </Grid>
-              <Grid item>
-                <Typography className={classes.name}>
-                  Christine Wilson
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography className={classes.profileText}>
-                  Toronto, Canada
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Button className={classes.sendMessageButton}>
-                  Send message
-                </Button>
-              </Grid>
+  const classes = themes();
+
+  const [user, setUser] = useState([]);
+  const [userFavCuisines, setUserFavCuisines] = useState(null);
+  useEffect(() => {
+    fetch("/users/" + getLogin())
+      .then((res) => res.json()) //should check if response status code 200 else return error
+      .then((data) => {
+        setUser(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (user.favCuisines) {
+      const fav = user.favCuisines.map((cuisine) => {
+        return <Grid item className={classes.cuisine}>{cuisine}</Grid>;
+      });
+      setUserFavCuisines(fav);
+    }
+  }, [user]);
+
+  return (
+    <Grid>
+      <Grid container spacing={2}>
+        <Grid item sm={4} container>
+          <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <Avatar className={classes.avatar} src={user.picture}></Avatar>
             </Grid>
-          </Grid>
-          <Grid item sm={8}>
-            <Grid container container direction="column" justify="center">
-              <Grid item>
-                <Typography className={classes.profileHeader}>
-                  ABOUT ME:
-                </Typography>
-                <Typography className={classes.profileText}>
-                  Hi everyone! I'm a foodie and I love to eat healthy and tasty
-                  meals. Also I'm mom of two beautiful babies.
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography className={classes.profileHeader}>
-                  FAVORITE CUISINE:
-                </Typography>
-              </Grid>
-              <Grid item>Japanese Chinese Mediterranean Thai</Grid>
+            <Grid item>
+              <Typography className={classes.name}>{user.name}</Typography>
+            </Grid>
+            <Grid item>
+              <Typography className={classes.profileText}>
+                {user.location}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Button className={classes.sendMessageButton}>
+                Send message
+              </Button>
             </Grid>
           </Grid>
         </Grid>
-        <Grid>map here</Grid>
+        <Grid item sm={8} container justify="center" alignItems="center">
+          <Grid container direction="column" justify="center" spacing={2}>
+            <Grid item>
+              <Typography className={classes.profileHeader}>
+                ABOUT ME:
+              </Typography>
+              <Typography className={classes.profileText}>
+                {user.aboutMe}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography className={classes.profileHeader}>
+                FAVORITE CUISINE:
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Grid container direction="row" justify="flex-start" spacing={2}>
+                {userFavCuisines}
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
       </Grid>
-    );
+      <Grid>map here</Grid>
+    </Grid>
+  );
 }
 
 export default UserProfile;

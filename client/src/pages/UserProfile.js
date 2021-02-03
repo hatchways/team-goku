67e8f927b5b19d7a6166ae357dbe49d66179e3cd
Tony Le <le.tony.05@gmail.com>
2021-02-02 19:44:55 -0500

    styling added in progress

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -1,15 +1,44 @@
 import React, { useState, useEffect } from "react";
-import { Grid, Avatar, Button } from "@material-ui/core";
+import { Grid, Avatar, Button, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles({
-    root: {
-        minHeight: "100vh"
+const themes = makeStyles((theme) => ({
+  root: {
+    minHeight: "100vh",
+  },
+  avatar: {
+    width: theme.spacing(25),
+    height: theme.spacing(25),
+  },
+  name: {
+    fontSize: "1.5em",
+    fontWeight: "500",
+  },
+  sendMessageButton: {
+    color: "#FF743D",
+    borderStyle: "solid",
+    borderWidth: "1px",
+    borderColor: "#FF743D",
+    textTransform: "none",
+    margin: "1em 0 1em 0",
+    borderRadius: "0 0 0 0",
+    padding: "2em 3em 2em 3em",
+    "&:hover": {
+      backgroundColor: "#FF743D",
+      color: "#fff",
     },
-});
+  },
+  profileHeader: {
+    fontWeight: "700",
+  },
+  profileText: {
+    fontWeight: "500",
+    color: "#777777",
+  },
+}));
 
 function UserProfile() {
-    const classes = useStyles();
+    const classes = themes();
     
     return (
       <Grid>
@@ -22,24 +51,41 @@ function UserProfile() {
               alignItems="center"
             >
               <Grid item>
-                <Avatar></Avatar>
+                <Avatar className={classes.avatar}></Avatar>
               </Grid>
-              <Grid item>Christine Wilson</Grid>
-              <Grid item>Toronto, Canada</Grid>
               <Grid item>
-                <Button>Send message</Button>
+                <Typography className={classes.name}>
+                  Christine Wilson
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography className={classes.profileText}>
+                  Toronto, Canada
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Button className={classes.sendMessageButton}>
+                  Send message
+                </Button>
               </Grid>
             </Grid>
           </Grid>
           <Grid item sm={8}>
-            <Grid
-              container
-              container
-              direction="column"
-              justify="center"
-            >
-              <Grid item>About me: Hi everyone! I'm a foodie and I love to eat healthy and tasty meals. Also I'm mom of two beautiful babies.</Grid>
-              <Grid item>Favourite Cuisine</Grid>
+            <Grid container container direction="column" justify="center">
+              <Grid item>
+                <Typography className={classes.profileHeader}>
+                  ABOUT ME:
+                </Typography>
+                <Typography className={classes.profileText}>
+                  Hi everyone! I'm a foodie and I love to eat healthy and tasty
+                  meals. Also I'm mom of two beautiful babies.
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography className={classes.profileHeader}>
+                  FAVORITE CUISINE:
+                </Typography>
+              </Grid>
               <Grid item>Japanese Chinese Mediterranean Thai</Grid>
             </Grid>
           </Grid>
